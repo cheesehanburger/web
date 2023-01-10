a34d1ef6ec1fd431654082a61af06d5a540d86c6
hanburger <cheesehanburger@163.com>
2023-01-10 15:20:31 +0800

    开始vue组件的学习

---
+++ practises/VUE/vue2.0/vue基础/code/demo-first/src/Test.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- 组成1:template -->
+  <!-- 注意:template中只能有一个根节点 -->
+  <div class="test-box">
+    <h3>这是用户自定义的Test.vue --我的名字是:{{ username }}</h3>
+    <button @click="changeName">点击换名</button>
+  </div>
+</template>
+
+<script>
+// 组成3:script
+// 固定写法:默认导出
+export default {
+  // data数据源
+  // 注意:.vue组件中的 data不能像之前一样指向对象
+  // 组件中的date必须是一个函数
+  data() {
+    // 这个return对象中,可以定义数据
+    return {
+      username: "han",
+    };
+  },
+  methods: {
+    changeName() {
+      this.username = "liu";
+      // vue组件里的this是组件的实例对象
+      console.log(this);
+    },
+  },
+  // 当前组件中的侦听器
+  watch: {},
+  // 当前组件中的计算属性
+  computed: {},
+};
+</script>
+
+<style lang='less'>
+/* 组成2:style */
+/* 注意:可以通过lang指定预处理器 */
+.test-box {
+  background-color: pink;
+  h3 {
+    color: red;
+  }
+}
+</style>
--- practises/VUE/vue2.0/vue基础/code/demo-first/src/main.js
+++ practises/VUE/vue2.0/vue基础/code/demo-first/src/main.js
@@ -1,8 +1,14 @@
+// 导入vue这个包,得到Vue构造函数
 import Vue from 'vue'
-import App from './App.vue'
+// 导入App.vue根组件,将来要把App.js中的模版结构,渲染到HTML页面中
+// import App from './App.vue'
+import Test from './Test.vue'
 
 Vue.config.productionTip = false
 
+// 创建Vue的实例对象
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  //render函数的作用就是,把指定组件渲染到HTML页面中，其中Test就是指定的组件,（**该组件叫做根组件**）
+  render: h => h(Test),
+  //$mount函数的作用就和el属性的作用一样，#app就是指定的页面
+}).$mount('#app') //#app就是指定的页面
